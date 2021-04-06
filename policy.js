bdf42a97652b99ad511c15c40bbbd4eb50135a60
atharva3899 <atharvapdeshpande@gmail.com>
2021-04-06 22:44:58 +0530

    improved policy page

--- policy.js
+++ policy.js
@@ -1,23 +1,72 @@
-const addTO = document.getElementById("addTo");
-console.log(addTO);
-fetch("data.json")
+var displayElems = () => {
+  const addTO = document.getElementById("addTo");
+  fetch("data.json")
   .then(response => response.json())
-  .then(json => {
-    console.log(json);
-    displayElems(json);
-  });
-
-var sortRating = () => {
-  const addto1 = document.getElementById("addTo");
-  console.log(addto1);
+  .then(jsonData => {
+    for(i=0;i<jsonData.length;i++) {
+      createDIV(addTO,jsonData,i);
+    }  
+  });  
 }
 
 
-var displayElems = (jsonData) => {
-  for(i=0;i<jsonData.length;i++) {
-    var div = document.createElement('div');
-    div.classList="row shadow m-3 p-3";
-    div.innerHTML = jsonData[i].Company + "<br>" + jsonData[i].fileName + "<br>" + jsonData[i].rating;
-    addTO.appendChild(div);
-  }
+var sortRating = () => {
+  const addTO = document.getElementById("addTo");
+  addTO.innerHTML = "";
+  fetch("data.json")
+  .then(response => response.json())
+  .then(jsonData => {
+    jsonData.sort((a,b) => (a.rating > b.rating) ? -1 : 1);
+    for(i=0;i<jsonData.length;i++) {
+      createDIV(addTO,jsonData,i);
+    }  
+  }); 
+}
+
+var sortName = () => {
+  const addTO = document.getElementById("addTo");
+  addTO.innerHTML = "";
+  fetch("data.json")
+  .then(response => response.json())
+  .then(jsonData => {
+    jsonData.sort((a,b) => (a.Company > b.Company) ? 1 : -1);
+    for(i=0;i<jsonData.length;i++) {
+      createDIV(addTO,jsonData,i);
+    }  
+  }); 
+}
+
+var createDIV = (addTO, jsonData, i) => {
+  // parent div
+  var div = document.createElement('div');
+  div.classList="policyCard rounded shadow my-3 p-3";
+  
+  // heading
+  var h3 = document.createElement('h3');
+  h3.innerText = jsonData[i].Company;
+
+  //rating 
+  var rate = document.createElement('b');
+  rate.innerText = "Rating: " + jsonData[i].rating;
+
+  //years
+  var yr = document.createElement('b');
+  yr.innerText = "Years: " + jsonData[i].years;
+
+  //details
+  var dets = document.createElement('p');
+  dets.innerText = jsonData[i].details;
+
+  //break
+  var br = document.createElement("br");
+
+  div.appendChild(h3);
+  div.appendChild(document.createElement("br"));
+  div.appendChild(rate);
+  div.appendChild(document.createElement("br"));
+  div.appendChild(yr);
+  div.appendChild(document.createElement("br"));
+  div.appendChild(dets);
+
+  addTO.appendChild(div);
 }
